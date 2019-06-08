9c5b74dfc3d5fb067c51b59a195e3260e60a64c9
Márcia Coutinho <marcia.silveiro@gmail.com>
2019-06-08 13:25:05 +0200

    Creating basic classes

---
+++ app/src/main/java/com/marciasc/languagegame/WordTranslation.java
@@ -0,0 +1,32 @@
+package com.marciasc.languagegame;
+
+public class WordTranslation {
+    private String mWord;
+    private String mTranslation;
+    private Boolean mRealPair;
+
+    public String getmWord() {
+        return mWord;
+    }
+
+    public void setmWord(String mWord) {
+        this.mWord = mWord;
+    }
+
+    public String getmTranslation() {
+        return mTranslation;
+    }
+
+    public void setmTranslation(String mTranslation) {
+        this.mTranslation = mTranslation;
+    }
+
+    public Boolean getmRealPair() {
+        return mRealPair;
+    }
+
+    public void setmRealPair(Boolean mRealPair) {
+        this.mRealPair = mRealPair;
+    }
+
+}
---
+++ app/src/main/java/com/marciasc/languagegame/game/GamePresenter.java
@@ -0,0 +1,25 @@
+package com.marciasc.languagegame.game;
+
+import com.marciasc.languagegame.WordTranslation;
+
+public class GamePresenter implements GameContract.Presenter {
+    @Override
+    public void setView(GameContract.View view) {
+
+    }
+
+    @Override
+    public void loadGame() {
+
+    }
+
+    @Override
+    public void initGame() {
+
+    }
+
+    @Override
+    public void markPoint(WordTranslation wordTranslation, boolean rightTranslation) {
+
+    }
+}
